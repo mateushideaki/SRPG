63d219cc8a2a06dd428fca478c1a2ef7480bc9bf
Hideaki <mateustaroda@gmail.com>
2017-07-05 01:34:10 -0300

    Licensa GPL - ajustes

--- src/srpg/Compare.java
+++ src/srpg/Compare.java
@@ -1,12 +1,12 @@
 /* <Sistema de Reconhecimento de Padrões utilizando Grafos.>
     Copyright (C) <2017>  <Mateus Hideaki Taroda> <mateustaroda@gmail.com>
 
-    This program is free software: you can redistribute it and/or modify
+    SRPG is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    SRPG is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
